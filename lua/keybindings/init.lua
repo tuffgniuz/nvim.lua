ab62ac5b2e20d4ae9f938d1242265cd397bbd5a1
Imanuel Febie <tuffgniuz.dev@gmail.com>
2021-12-14 16:12:12 +0700

    changed keybindings for lsp/lspsaga

--- lua/keybindings/init.lua
+++ lua/keybindings/init.lua
@@ -4,7 +4,7 @@ local opts = { noremap = true, silent = true }
 vim.g.mapleader = ' '   -- leader key
 
 -- nvim tree mappings
-map('n', '<leader>e', ':NvimTreeToggle<CR>', opts) 
+map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 -- telescope mappings
 map('n', '<leader>ff', ':Telescope find_files<cr>', opts)
 map('n', '<leader>fg', ':Telescope live_grep<cr>', opts)
@@ -13,7 +13,7 @@ map('n', '<leader>fb', ':Telescope buffers<cr>', opts)
 map('n', '<A-,>', ':BufferPrevious<CR>', opts)
 map('n', '<A-.>', ':BufferNext<CR>', opts)
 map('n', '<A-<>', ':BufferMovePrevious<CR>', opts)
-map('n', '<A->>', ' :BufferMoveNext<CR>', opts)
+map('n', '<A->>', ':BufferMoveNext<CR>', opts)
 map('n', '<A-1>', ':BufferGoto 1<CR>', opts)
 map('n', '<A-2>', ':BufferGoto 2<CR>', opts)
 map('n', '<A-3>', ':BufferGoto 3<CR>', opts)
@@ -29,5 +29,20 @@ map('n', '<C-p>', ':BufferPick<CR>', opts)
 map('n', '<Space>bb', ':BufferOrderByBufferNumber<CR>', opts)
 map('n', '<Space>bd', ':BufferOrderByDirectory<CR>', opts)
 map('n', '<Space>bl', ':BufferOrderByLanguage<CR>', opts)
+-- See `:help vim.lsp.*` for documentation on any of the below functions
+map('n', '<leader>D', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+map('n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+map('n', '<leader>h', '<cmd>Lspsaga hover_doc<CR>', opts)
+map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+map('n', '<leader>t', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+map('n', '<C-e>', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
 
 
