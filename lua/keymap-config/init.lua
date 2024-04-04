c173e2c9603c5ab77e14e8319faac922e2c93b63
Imanuel Febie <tuffgniuz.dev@gmail.com>
2024-04-05 01:06:33 +0200

    Added missing keymap config

---
+++ lua/keymap-config/init.lua
@@ -0,0 +1,46 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = " " -- leader key
+
+--> oil
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+--> neogit <--
+map("n", "ng", ":Neogit<CR>", {})
+--> diffview <--
+map("n", "<leader>do", ":DiffviewOpen<CR>", {})
+map("n", "<leader>dc", ":DiffviewClose<CR>", {})
+map("n", "<leader>dt", ":DiffviewToggleFiles<CR>", {})
+--> truezen <--
+map("n", "<leader>za", ":TZAtaraxis<CR>", {})
+map("n", "<leader>zm", ":TZMinimalist<CR>", {})
+--> nvim tree mappings <--
+map("n", "<leader>n", ":NvimTreeToggle<CR>", opts)
+map("n", "<leader>gn", ":NvimTreeFocus<CR>", opts)
+--> telescope mappings <--
+map("n", "<leader>fd", ":Telescope diagnostics<cr>", opts)
+map("n", "<leader>ff", ":Telescope find_files<cr>", opts)
+map("n", "<leader>fr", ":Telescope lsp_references<cr>", opts)
+map("n", "<leader>oc", ":Telescope lsp_outgoing_calls<cr>", opts)
+map("n", "<leader>fg", ":Telescope live_grep<cr>", opts)
+map("n", "<leader>fb", ":Telescope buffers<cr>", opts)
+--> barbar mappings <--
+map("n", "<A-,>", ":BufferPrevious<CR>", opts)
+map("n", "<A-.>", ":BufferNext<CR>", opts)
+map("n", "<A-<>", ":BufferMovePrevious<CR>", opts)
+map("n", "<A->>", ":BufferMoveNext<CR>", opts)
+map("n", "<A-1>", ":BufferGoto 1<CR>", opts)
+map("n", "<A-2>", ":BufferGoto 2<CR>", opts)
+map("n", "<A-3>", ":BufferGoto 3<CR>", opts)
+map("n", "<A-4>", ":BufferGoto 4<CR>", opts)
+map("n", "<A-5>", ":BufferGoto 5<CR>", opts)
+map("n", "<A-6>", ":BufferGoto 6<CR>", opts)
+map("n", "<A-7>", ":BufferGoto 7<CR>", opts)
+map("n", "<A-8>", ":BufferGoto 8<CR>", opts)
+map("n", "<A-9>", ":BufferGoto 9<CR>", opts)
+map("n", "<A-0>", ":BufferLast<CR>", opts)
+map("n", "<A-c>", ":BufferClose<CR>", opts)
+map("n", "<C-p>", ":BufferPick<CR>", opts)
+map("n", "<leader>bb", ":BufferOrderByBufferNumber<CR>", opts)
+map("n", "<leader>bd", ":BufferOrderByDirectory<CR>", opts)
+map("n", "<leader>bl", ":BufferOrderByLanguage<CR>", opts)
